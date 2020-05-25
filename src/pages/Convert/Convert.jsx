f478deb79850a339e83d544ae951144c1fb27539
Frank Rauen <home@Franks-MacBook-Air.local>
2020-05-25 13:05:21 -0600

    adds API to Convert functin

--- src/pages/Convert/Convert.jsx
+++ src/pages/Convert/Convert.jsx
@@ -5,6 +5,20 @@ class Convert extends Component {
     state = {
         country: [],
     }
+
+    handleGetValue = async searchTerm => {
+        const API_KEY = '526af95664f4a35c896d7ff5a3330e3b'
+        try {
+        const apiUrl = `https://data.fixer.io/api/latest?access_key=${API_KEY}&from=GBP&to=JPY&amount=25`
+        const response = await fetch(apiUrl);
+        const {country} = await response.json();
+        this.setState({country})
+        } catch (error) {
+            alert("COUNTRY NOT FOUND");
+        }
+    }
+
+
     render() {
         return(
             <p>Conversion of one currency value to another</p>
--- src/pages/Lookup/Lookup.jsx
+++ src/pages/Lookup/Lookup.jsx
@@ -5,6 +5,16 @@ class Lookup extends Component {
     state = {
         country: [],
     }
+
+    handleGetValue = async searchTerm => {
+        const API_KEY = '526af95664f4a35c896d7ff5a3330e3b'
+        try {
+        const apiUrl = `https://data.fixer.io/api/latest?access_key=${API_KEY}&base=USD`
+        } catch (error) {
+            alert("COUNTRY NOT FOUND");
+        }
+    }
+
     render() {
         return(
             <p>Currency Value Lookup</p>
